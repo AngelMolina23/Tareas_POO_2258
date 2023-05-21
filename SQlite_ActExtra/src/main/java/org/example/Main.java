e574d93b5bc70034ac027298fadf221798b7a028
AngelMolina23 <125847601+AngelMolina23@users.noreply.github.com>
2023-05-21 13:58:46 -0600

    Add files via upload

--- SQlite_ActExtra/src/main/java/org/example/Main.java
+++ SQlite_ActExtra/src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.controlador.ControladorLibro;
+import org.example.vista.VentanaLibro;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        VentanaLibro view = new VentanaLibro("Actividades extra 7,8 y 9");
+        ControladorLibro controller = new ControladorLibro(view);
+        }
+
     }
-}
+
